ad0c281eaf6125c04ae92b4f7b1eea3fb48aa451
KRITIKA <kritikakumawatkk2001@gmail.com>
2022-03-24 15:12:50 +0530

    Day 2 Completed

---
+++ lib/home_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Flutter Catalog"),
+      ),
+        body: Center(
+            child: Container(
+      child: Text("Welcome to 30 days of Flutter!"),
+    ),
+    ),
+    drawer: Drawer(),
+    );
+  }
+}
