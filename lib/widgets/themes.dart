3a7609704a6b8ba61045c9d13ec076cd410a147a
KRITIKA <kritikakumawatkk2001@gmail.com>
2022-04-16 21:21:10 +0530

    Day 17 Completed

--- lib/widgets/themes.dart
+++ lib/widgets/themes.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 class MyTheme {
   static ThemeData lightTheme(BuildContext context) => ThemeData(
       primarySwatch: Colors.deepPurple,
+      //fontFamily: GoogleFonts.poppins().fontFamily,
       appBarTheme: AppBarTheme(
         color: Colors.white,
         elevation: 0.0,
@@ -14,4 +15,7 @@ class MyTheme {
   static ThemeData darkTheme(BuildContext context) => ThemeData(
         brightness: Brightness.dark,
       );
+
+  static Color creamColor = const Color(0xfff5f5f5);
+  static Color darkBluishColor = const Color(0xff403b58);
 }
